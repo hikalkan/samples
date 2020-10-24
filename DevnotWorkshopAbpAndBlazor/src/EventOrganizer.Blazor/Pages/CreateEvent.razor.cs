e4e4424817e120e483f0aec8f3b0f9f9f8e043ed
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-10-24 22:14:03 +0300

    Created initial "event creation" page

--- DevnotWorkshopAbpAndBlazor/src/EventOrganizer.Blazor/Pages/CreateEvent.razor.cs
+++ DevnotWorkshopAbpAndBlazor/src/EventOrganizer.Blazor/Pages/CreateEvent.razor.cs
@@ -1,7 +1,19 @@
-﻿namespace EventOrganizer.Blazor.Pages
+﻿using System.Threading.Tasks;
+using EventOrganizer.Events;
+using Microsoft.AspNetCore.Components;
+
+namespace EventOrganizer.Blazor.Pages
 {
     public partial class CreateEvent
     {
+        private EventCreationDto Event { get; set; } = new EventCreationDto();
 
+        [Inject]
+        private IEventAppService EventAppService { get; set; }
+
+        private async Task Create()
+        {
+            await EventAppService.CreateAsync(Event);
+        }
     }
 }
