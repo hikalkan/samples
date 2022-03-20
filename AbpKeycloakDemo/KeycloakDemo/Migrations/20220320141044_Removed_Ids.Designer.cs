ac1bb6b05898c01ae3bb9b493254343dda7a5d7a
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2022-03-20 17:11:15 +0300

    Remove IDS-related tables.

--- AbpKeycloakDemo/KeycloakDemo/Migrations/20220320141044_Removed_Ids.Designer.cs
+++ AbpKeycloakDemo/KeycloakDemo/Migrations/20220320141044_Removed_Ids.Designer.cs
@@ -4,6 +4,7 @@ using KeycloakDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace KeycloakDemo.Migrations
 {
     [DbContext(typeof(KeycloakDemoDbContext))]
-    partial class KeycloakDemoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220320141044_Removed_Ids")]
+    partial class Removed_Ids
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
